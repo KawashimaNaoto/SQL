e7c5e9131e1d730c97f1cc30c17eca9d8a68357e
DESKTOP-JK415VH\naxtt <naoto.kawashima.likeit@gmail.com>
2018-11-08 17:12:59 +0900

    SQL課題

---
+++ Q1.sql
@@ -0,0 +1,3 @@
+CREATE TABLE item_category(
+category_id SERIAL,
+category_name varchar(256)NOT NULL);
---
+++ Q2.sql
@@ -0,0 +1,5 @@
+CREATE TABLE item(
+item_id SERIAL,
+item_name varchar(256)NOT NULL,
+item_price int NOT NULL,
+category_id int);
